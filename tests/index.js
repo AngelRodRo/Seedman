b6d7172e174d891a92154915c7e4e5427613421c
Angel <aleangrodriguez123@gmail.com>
2019-02-20 20:41:32 -0500

    Added tests

--- tests/index.js
+++ tests/index.js
@@ -8,6 +8,10 @@ describe("bale", () => {
         bale = new Bale();
     });
 
+    afterEach(() => {
+        bale.seeders = [];
+    });
+
     describe("drivers", () => {
         describe("mongo", () => {
             const MongoDriver = require("../src/drivers/mongo");
@@ -76,6 +80,24 @@ describe("bale", () => {
             expect(bale.seeders[0]).to.have.property("properties");
             expect(bale.seeders[0]).to.have.property("count");
         });
+
+        it("should show a message for avoid insert fake data", async () => {
+            const seed = {
+                name: "",
+                properties: {
+                    name: ""
+                },
+                count: 1
+            };
+
+
+            bale.use(seed);
+            await bale.connect({});
+            console.log(bale)
+            const success = await bale.run()
+            expect(success).to.equal(false);
+           
+        })
 
         it("should add fake data", async () => {
             const seed = {
